45682409e4624da80fe88f1b2fb919d3c0c67db9
liam <liachenxiexu@gmail.com>
2021-08-20 14:53:07 +1000

    remove build

--- build/analyzers/WinsAnalyzer.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WinsAnalysis = void 0;
-var WinsAnalysis = /** @class */ (function () {
-    function WinsAnalysis() {
-    }
-    return WinsAnalysis;
-}());
-exports.WinsAnalysis = WinsAnalysis;
